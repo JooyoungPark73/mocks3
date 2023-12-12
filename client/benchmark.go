c8916ad675e368d5cde29359133f81d0a59904ca
JooYoung Park <jooyoung.park73@gmail.com>
2023-12-12 13:58:22 +0800

    Add benchmarking functionality for client get and put operations
    Signed-off-by: JooYoung Park <jooyoung.park73@gmail.com>

---
+++ client/benchmark.go
@@ -0,0 +1,64 @@
+package mocks3
+
+import (
+	"encoding/csv"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func BenchmarkClientGet(testIteration int) {
+	// Setup any required resources (like a mock server)
+	csvFile, err := os.Create("get_benchmark.csv")
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+	csvwriter := csv.NewWriter(csvFile)
+	defer csvwriter.Flush()
+	err = csvwriter.Write([]string{"Payload Size (Bytes)", "E2E Time (ms)", "Target Time (ms)"})
+	if err != nil {
+		log.Fatalf("could not write to CSV file: %v", err)
+	}
+
+	for i := 0; i < testIteration; i++ {
+		randNumber := rand.Float64() * 29
+		payloadSize := int64(math.Pow(2, randNumber))
+		e2eTime, targetTime := ClientGet(payloadSize, "none")
+		err := csvwriter.Write([]string{strconv.FormatInt(payloadSize, 10), strconv.FormatInt(e2eTime, 10), strconv.FormatInt(targetTime, 10)})
+		if err != nil {
+			log.Fatalf("could not write to CSV file: %v", err)
+		}
+		csvwriter.Flush()
+	}
+	csvFile.Close()
+	// Teardown any resources
+}
+
+func BenchmarkClientPut(testIteration int) {
+	// Setup any required resources (like a mock server)
+	csvFile, err := os.Create("put_benchmark.csv")
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+	csvwriter := csv.NewWriter(csvFile)
+	defer csvwriter.Flush()
+	err = csvwriter.Write([]string{"Payload Size (Bytes)", "E2E Time (ms)", "Target Time (ms)"})
+	if err != nil {
+		log.Fatalf("could not write to CSV file: %v", err)
+	}
+
+	for i := 0; i < testIteration; i++ {
+		randNumber := rand.Float64() * 29
+		payloadSize := int64(math.Pow(2, randNumber))
+		e2eTime, targetTime := ClientPut(payloadSize, "none")
+		err := csvwriter.Write([]string{strconv.FormatInt(payloadSize, 10), strconv.FormatInt(e2eTime, 10), strconv.FormatInt(targetTime, 10)})
+		if err != nil {
+			log.Fatalf("could not write to CSV file: %v", err)
+		}
+		csvwriter.Flush()
+	}
+	csvFile.Close()
+}
--- client/client_put.go
+++ client/client_put.go
@@ -2,12 +2,9 @@ package mocks3
 
 import (
 	"context"
-	"encoding/csv"
 	"flag"
 	"math"
-	"math/rand"
 	"os"
-	"strconv"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -81,30 +78,4 @@ func ClientPut(size int64, addr string) (int64, int64) {
 	e2eTime := time.Now().UnixMicro() - sendTime
 
 	return e2eTime / 1000, targetTime.Milliseconds()
-}
-
-func BenchmarkClientPut(testIteration int) {
-	// Setup any required resources (like a mock server)
-	csvFile, err := os.Create("put_benchmark.csv")
-	if err != nil {
-		log.Fatalf("failed creating file: %s", err)
-	}
-	csvwriter := csv.NewWriter(csvFile)
-	defer csvwriter.Flush()
-	err = csvwriter.Write([]string{"Payload Size (Bytes)", "E2E Time (ms)", "Target Time (ms)"})
-	if err != nil {
-		log.Fatalf("could not write to CSV file: %v", err)
-	}
-
-	for i := 0; i < testIteration; i++ {
-		randNumber := rand.Float64() * 29
-		payloadSize := int64(math.Pow(2, randNumber))
-		e2eTime, targetTime := ClientPut(payloadSize, "none")
-		err := csvwriter.Write([]string{strconv.FormatInt(payloadSize, 10), strconv.FormatInt(e2eTime, 10), strconv.FormatInt(targetTime, 10)})
-		if err != nil {
-			log.Fatalf("could not write to CSV file: %v", err)
-		}
-		csvwriter.Flush()
-	}
-	csvFile.Close()
 }
